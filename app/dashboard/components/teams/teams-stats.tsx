17bcadaf07c6d5d35bdbb84350244f03c6c9bca7
Gaurav Jangid <jangidgaurav603@gmail.com>
2025-11-29 23:35:29 +0530

    Add the full line chart

--- app/dashboard/components/teams/teams-stats.tsx
+++ app/dashboard/components/teams/teams-stats.tsx
@@ -10,6 +10,7 @@ import rl from "@/public/images/rl.jpg"
 import { TooltipProvider , Tooltip, TooltipTrigger, TooltipContent } from "@/components/ui/tooltip"
 import { Avatar , AvatarFallback } from "@/components/ui/avatar"
 import TeamDistributionChart from "./team-distribution-chart"
+import SupportTicketsResolved from "./support-tickets-resolved"
 
 
 const teamLeaders = [
@@ -63,13 +64,13 @@ const TeamsStats = () => {
         <div className="grid lg:grid-cols-3 gap-4">
          {/* Card 1 */}
         <Card>
-            <CardHeader >
+            <CardHeader  >
                 <CardTitle className="text-base ">
                     Total teams
                 </CardTitle>
             </CardHeader>
             <CardContent className="flex justify-between items-center">
-                <div className="flex gap-2">
+                <div className="flex gap-3">
                     <UsersIcon /> 
                     <span className="text-5xl font-bold">8</span>
                 </div>
@@ -82,7 +83,7 @@ const TeamsStats = () => {
         </Card>
         {/* Card 2 */}
         <Card>
-            <CardHeader className="pb-2">
+            <CardHeader >
                 <CardTitle className="text-base flex justify-between items-center">
                     <span>Teams leaders </span>
                     <StarIcon className="text-yellow-500" />
@@ -115,13 +116,13 @@ const TeamsStats = () => {
         </Card>
         {/* Card 3 */}
         <Card className="flex flex-col">
-            <CardHeader>
+            <CardHeader >
                   <CardTitle className="text-base flex justify-between items-center">
                     <span>Teams distribution </span>
                     <PieChart />
                 </CardTitle>
             </CardHeader>
-            <CardContent>
+            <CardContent className="pb-0">
                 {/* Pie Chart in a card */}
                 <TeamDistributionChart />
             </CardContent>
@@ -139,7 +140,7 @@ const TeamsStats = () => {
             </CardHeader>
             <CardContent className="pl-0">
                 {/* Pie chart component */}
-                line graph
+                <SupportTicketsResolved />
             </CardContent>
         </Card>
 
