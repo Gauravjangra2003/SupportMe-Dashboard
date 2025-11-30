76a20d330708a96c8e0f80c144ff91c7fdbbd800
Gaurav Jangid <jangidgaurav603@gmail.com>
2025-11-30 20:57:49 +0530

    Add hamburger mobile menu

--- app/dashboard/components/menu-item.tsx
+++ app/dashboard/components/menu-item.tsx
@@ -11,12 +11,14 @@ type MenuItemProps = {
 
 const MenuItem = ({children, href} : MenuItemProps) => {
 
+
   const pathname = usePathname();
   const isActive = pathname === href;
 
   return (
    <li>
-     <Link className={cn("block p-2 hover:bg-white dark:hover:bg-zinc-700 rounded-md text-muted-foreground hover:text-foreground" , isActive && "bg-primary hover:bg-primary dark:hover:bg-primary hover:text-primary-foreground text-primary-foreground")} href={href}>{children}</Link>
+     <Link className={cn("block p-2 hover:bg-white dark:hover:bg-zinc-700 rounded-md text-muted-foreground hover:text-foreground" , isActive && "bg-primary hover:bg-primary dark:hover:bg-primary hover:text-primary-foreground text-primary-foreground")} href={href}
+        >{children}</Link>
    </li>
   )
 }
